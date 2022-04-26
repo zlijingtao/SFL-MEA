a6b32bea3547692166406bce6529a8938d4e42be
Jingtao Li - jingtao1@asu.edu -  - Grad Research Assistant - Graduate Student - Student - Sch Elect Comptr Jingtao1 Energy Engr <yxiong35@asu.edu>
2022-04-25 18:30:14 -0700

    update resnet-figure5 scripts

---
+++ steal_exp/ace_run_figure5_resnet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+bash figure5_resnet20/ace_run_resnet20_cifar10_train_craft.sh
+bash figure5_resnet20/ace_run_resnet20_cifar10_train_craft_resume.sh
+bash figure5_resnet20/ace_run_resnet20_cifar10_train_gan.sh
+bash figure5_resnet20/ace_run_resnet20_cifar10_train_gan_resume.sh
+bash figure5_resnet20/ace_run_resnet20_cifar10_train_GM.sh
+bash figure5_resnet20/ace_run_resnet20_cifar10_train_GM_resume.sh
+bash figure5_resnet20/ace_run_resnet20_cifar10_train_soft.sh
+bash figure5_resnet20/ace_run_resnet20_cifar10_train_soft_resume.sh
