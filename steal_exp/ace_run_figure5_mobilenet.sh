c228b9a2fa720d36bea31ee7a3db8ba9a823653c
Jingtao Li - jingtao1@asu.edu -  - Grad Research Assistant - Graduate Student - Student - Sch Elect Comptr Jingtao1 Energy Engr <lijingtaoz@hotmail.com>
2022-04-28 15:45:56 -0700

    update from lab 3090

--- steal_exp/ace_run_figure5_mobilenet.sh
+++ steal_exp/ace_run_figure5_mobilenet.sh
@@ -3,7 +3,9 @@ cd "$(dirname "$0")"
 
 # bash figure5_mobilenet/ace_run_mobilenet_cifar10_train_craft.sh
 bash figure5_mobilenet/ace_run_mobilenet_cifar10_train_craft_resume.sh
-# bash figure5_mobilenet/ace_run_mobilenet_cifar10_train_gan.sh
+bash figure5_vgg11_cifar100/ace_run_vgg11_cifar10_train_gan.sh
+bash figure5_vgg11_cifar100/ace_run_vgg11_cifar10_train_gan_resume.sh
+bash figure5_mobilenet/ace_run_mobilenet_cifar10_train_gan.sh
 bash figure5_mobilenet/ace_run_mobilenet_cifar10_train_gan_resume.sh
 bash figure5_mobilenet/ace_run_mobilenet_cifar10_train_GM.sh
 bash figure5_mobilenet/ace_run_mobilenet_cifar10_train_GM_resume.sh
