1c4d907b604a3e3a21a939e1ecc6bdc2a2b0f14b
Jingtao Li - jingtao1@asu.edu -  - Grad Research Assistant - Graduate Student - Student - Sch Elect Comptr Jingtao1 Energy Engr <lijingtaoz@hotmail.com>
2022-05-10 18:02:56 -0700

    update scripts

--- reproduce/ace_run_table3.sh
+++ reproduce/ace_run_table3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-bash table2/ace_run_steal_attack_softME_vgg11bn_cifar10.sh
-bash table2/ace_run_steal_attack_TrainME_vgg11bn_cifar10.sh
-bash table2/ace_run_vgg11_cifar10_train_softtrain.sh
-bash table2/ace_run_vgg11_cifar10_train_softtrain_resume.sh
+bash table3/ace_run_steal_attack_softME_vgg11bn_cifar10.sh
+bash table3/ace_run_steal_attack_TrainME_vgg11bn_cifar10.sh
+bash table3/ace_run_vgg11_cifar10_train_softtrain.sh
+bash table3/ace_run_vgg11_cifar10_train_softtrain_resume.sh
