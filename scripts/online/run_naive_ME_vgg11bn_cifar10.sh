275410f193b2c16090e76e3e6814005783ad36c2
zlijingtao <lijingtaoz@hotmail.com>
2023-05-05 00:04:06 -0700

    update strength to lambda when controling gan assit ME

--- scripts/online/run_naive_ME_vgg11bn_cifar10.sh
+++ scripts/online/run_naive_ME_vgg11bn_cifar10.sh
@@ -16,18 +16,18 @@ attack_client=0
 
 regularization=naive_train_ME_start0
 # cutlayer_list="10 11 12 13"
-cutlayer_list="10"
-regularization_strength_list="1.0"
+cutlayer_list="11 12 13"
+regularization_strength=1.0
 # num_client_list="5 10"
 num_client_list="5"
 noniid_ratio_list="1.0"
 last_n_batch_list="0"
-last_client_fix_amount=1000
+last_client_fix_amount_list="200 500 1000 2000"
 for random_seed in $random_seed_list; do
         for num_client in $num_client_list; do
                 for cutlayer in $cutlayer_list; do
                         for noniid_ratio in $noniid_ratio_list; do
-                                for regularization_strength in $regularization_strength_list; do
+                                for last_client_fix_amount in $last_client_fix_amount_list; do
                                         for last_n_batch in $last_n_batch_list; do
                                         folder_name="saves/train-ME-new"
                                         filename="vgg11-cifar10-$regularization-step$regularization_strength-cut$cutlayer-client$num_client-noniid$noniid_ratio--data$last_client_fix_amount"
@@ -35,6 +35,7 @@ for random_seed in $random_seed_list; do
                                                 --folder $folder_name --filename=$filename --num_client=$num_client --num_epochs=$num_epochs \
                                                 --dataset=$dataset --noniid_ratio=$noniid_ratio --scheme=$scheme  --learning_rate=$learning_rate --learning_rate_MEA=$learning_rate_MEA --attack_epochs=$attack_epochs \
                                                 --attack_client=$attack_client  --regularization=$regularization  --regularization_strength=$regularization_strength --last_n_batch=$last_n_batch --last_client_fix_amount=$last_client_fix_amount
+                                                
                                         done
                                 done
                         done
