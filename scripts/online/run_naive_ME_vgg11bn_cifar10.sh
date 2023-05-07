6b5466210b8a0f4a51d636eb54d69ea9445166e4
Jingtao Li - jingtao1@asu.edu -  - Grad Research Assistant - Graduate Student - Student - Sch Elect Comptr Jingtao1 Energy Engr <lijingtaoz@hotmail.com>
2023-05-06 18:29:09 -0700

    update scripts

--- scripts/online/run_naive_ME_vgg11bn_cifar10.sh
+++ scripts/online/run_naive_ME_vgg11bn_cifar10.sh
@@ -5,7 +5,7 @@ GPU_id=0
 arch=vgg11_bn
 batch_size=128
 num_epochs=200
-random_seed_list="123"
+random_seed_list="125"
 
 scheme=V2
 dataset=cifar10
@@ -24,14 +24,14 @@ num_client_list="5"
 noniid_ratio_list="1.0"
 last_n_batch_list="0"
 # last_client_fix_amount_list="200 500 1000 2000"
-last_client_fix_amount_list="200"
+last_client_fix_amount_list="50 100 200"
 for random_seed in $random_seed_list; do
         for num_client in $num_client_list; do
                 for cutlayer in $cutlayer_list; do
                         for noniid_ratio in $noniid_ratio_list; do
                                 for last_client_fix_amount in $last_client_fix_amount_list; do
                                         for last_n_batch in $last_n_batch_list; do
-                                        folder_name="saves/train-ME-new"
+                                        folder_name="saves/train-ME-seed125"
                                         filename="vgg11-cifar10-$regularization-step$regularization_strength-cut$cutlayer-client$num_client-noniid$noniid_ratio--data$last_client_fix_amount"
                                         CUDA_VISIBLE_DEVICES=$GPU_id python main_steal_online.py   --arch=$arch --cutlayer=$cutlayer --batch_size=$batch_size \
                                                 --folder $folder_name --filename=$filename --num_client=$num_client --num_epochs=$num_epochs \
